c36051f02bcd2fc112f751aaa8e6dff816d641b0
Abhishek Lal <abhisheklalnediya@gmail.com>
2019-05-21 20:21:42 +0530

    static pages

--- plaid_django/plaid_django/settings.py
+++ plaid_django/plaid_django/settings.py
@@ -132,8 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Rest Framework
 REST_FRAMEWORK = {
